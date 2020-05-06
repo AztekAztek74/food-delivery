06b8ebc7f2d207e9ffb67dac4581d307a85da8c9
Aztek <sizyh.nikita@bk.ru>
2020-05-06 19:09:16 +0700

    Updates

---
+++ precache-manifest.7589f5d3d6264d4cfe00f0a80fef9b3b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ad6a20201d9f218982113df3f525d8",
+    "url": "/food-delivery/index.html"
+  },
+  {
+    "revision": "13d996d9cf1b5506b02b",
+    "url": "/food-delivery/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "52b96832fbd3cc0072f3",
+    "url": "/food-delivery/static/css/main.62174f99.chunk.css"
+  },
+  {
+    "revision": "13d996d9cf1b5506b02b",
+    "url": "/food-delivery/static/js/2.91032f3f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/food-delivery/static/js/2.91032f3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52b96832fbd3cc0072f3",
+    "url": "/food-delivery/static/js/main.34c22181.chunk.js"
+  },
+  {
+    "revision": "fa0928b5afe201917945",
+    "url": "/food-delivery/static/js/runtime-main.3f302071.js"
+  },
+  {
+    "revision": "d5258113921876591a5f6dce002dc933",
+    "url": "/food-delivery/static/media/pasta.d5258113.jpg"
+  },
+  {
+    "revision": "8fd3e8bb6a5f9eac678ab2a8d8807cae",
+    "url": "/food-delivery/static/media/pizza.8fd3e8bb.jpg"
+  },
+  {
+    "revision": "5fd7b983e2e4b22212257c8237d9d384",
+    "url": "/food-delivery/static/media/soup.5fd7b983.jpg"
+  }
+]);
